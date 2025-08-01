6d8b6cfed31934a67431d1a716b3b181d1c70f1d
shashikalaayeshani86 <shashikalaayeshani86@gmail.com>
2025-08-01 12:09:45 +0530

    Last

--- Server/Src/server.ts
+++ Server/Src/server.ts
@@ -1,5 +1,35 @@
 import express from 'express';
 import http from 'http';
-import cors from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import { WebSocket, WebSocketServer } from 'ws';
+
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({port:4000});
+
+wss.on("connection",(ws:WebSocket)=>{
+    console.log("New Client Connected");
+});
+
+wss.on("message",(ws)=>{
+    console.log("Recieved a message from the client");
+    wss.clients.forEach((client)=>{
+        if((client !== ws) && (client.readyState === WebSocket.OPEN)){
+            client.send((ws));
+        }
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT,()=>{
+    console.log("Running on port "+ PORT);
+})
